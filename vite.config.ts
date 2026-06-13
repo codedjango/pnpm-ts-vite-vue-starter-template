e08cb0886e4adf87f8d12c8b38d8bbdf52b55b9c
Aniket Aryamane <aryamane.aniket@gmail.com>
2026-06-13 07:09:46 +0530

    Add server config in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -13,4 +13,17 @@ export default defineConfig({
 			'@': path.resolve(__dirname, './src'),
 		},
 	},
+	// server: {
+	// 	proxy: {
+	// 		'/api': {
+	// 			target: 'http://127.0.0.1:8000',
+	// 			changeOrigin: true,
+	// 			// rewrite: (r) => r.replace(/^\/api/, ''),
+	// 			headers: {
+	// 				Accept: 'application/json',
+	// 				'Content-Type': 'application/json',
+	// 			},
+	// 		},
+	// 	},
+	// },
 })
